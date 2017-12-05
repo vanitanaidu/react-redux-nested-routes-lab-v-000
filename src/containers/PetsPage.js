9c67a19f7a8df1c430e91dc7bc46cfc1fd859060
vanitanaidu <vanita112003@gmail.com>
2017-12-04 19:59:19 -0500

    Done.

--- src/containers/PetsPage.js
+++ src/containers/PetsPage.js
@@ -13,8 +13,18 @@ class PetsPage extends Component {
   }
 
   render() {
+    const { match, pets } = this.props
     return (
-      <div>Pets Page</div>
+      <div>
+      <h2> Pets Page </h2>
+      <Switch>
+        <Route path={`${match.url}/pets`} component={PetsPage} />
+        <Route path={`${match.url}/:petId`} component={PetsShow}/>
+        <Route exact path={match.url} render={() => (
+          <h3>Please select a Pet from the list.</h3>
+        )} />
+      </Switch>
+      </div>
     )
   }
 };
